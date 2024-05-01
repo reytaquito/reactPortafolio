90b1a637020ce5036d8cd088392939b89c0dd21b
reytaquito <reytaquito4@gmail.com>
2024-05-01 13:32:56 -0600

    experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -13,7 +13,7 @@ const ExperienceCard = ({ experience }) => (
     date={experience.date}
     iconStyle={{ background: experience.iconBg }}
     icon={
-      <div>
+      <div className="flex justify-center items-center w-full h-full ">
         <img src={experience.icon} alt={experience.company_name}
           className="w-[60%] h-[60%] object-contain" />
       </div>
@@ -21,9 +21,18 @@ const ExperienceCard = ({ experience }) => (
   >
     <div>
       <h3 className="text-white text-[24px] font-bold">
-{experience.title}
+        {experience.title}
       </h3>
+      <p className="text-secondary text-[16px] font-semibold " style={{ margin: 0 }}>{experience.company_name}</p>
     </div>
+    <ul className="mt-5 list-disc ml-5 space-y-2">
+      {experience.points.map((point, index) => (
+        <li key={`experince-point-${index}`}
+          className="text-white-100 text-[14px] pl-1 tracking-wider">
+          {point}
+        </li>
+      ))}
+    </ul>
   </VerticalTimelineElement>
 )
 const Experience = () => {
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
-
-const Ball = () => {
+import React, {Suspense} from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei'
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl])
+  
   return (
     <div>Ball</div>
+  )
+}
+
+const BallCanvas = ({icon}) => {
+  return(
+    <Canvas
+      frameloop='demand'
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false}/>
+        <Ball />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
   )
 }
 
